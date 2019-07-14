40fa120e745c24a4cd25c8febb2f0971250a117e
Jacob Sitowski <jacob.sitowski@gmail.com>
2019-07-14 19:37:36 -0400

    Cleaned up scripts.js, first test is working. Added some notes.

--- scripts/threads.js
+++ scripts/threads.js
@@ -2,40 +2,33 @@ var query = {
   "v": 3,
   "q": {
     "find": {
-    "out.s1": "1PLKxzCP7MNVKT7bW5JzAig1VTwMxQKJrR" },
+    "out.s1": "13jgh5vQC5a43rvawKVourhCY7JiCaDpPH" },
     "limit": 30,
   },
   "r": {
-    "f": "[ .[0] | { txid: .tx.h, timestamp: .blk.t?, content: .out[0]?.s2 }]"
+    "f": "[ .[0] | { txid: .tx.h, timestamp: .blk.t?, content: .out[0]?.s3 }]"
 
   }
 }
-// parse from genesis
+// fetch from genesis
 var b64 = btoa(JSON.stringify(query));
 var url = "https://genesis.bitdb.network/q/1FnauZ9aUH2Bex6JzdcV4eNX7oLSSEbxtN/" + b64;
 var header = {
   headers: { key: "14QX7pn5GbipWvNLyDfrdcZLzwvPYJSnhB" }
 };
+// parse it
 fetch(url, header).then(function(r) {
   return r.json()
 }).then(function(r) {
   r.c.forEach(function(output) {
     var threadid = output.txid;
-    console.log(`txid is ${threadid} `)
-    var post = document.createElement("post");
-    post.innerHTML =
-    " <i>Post ID:</i>  " +
-     output.txid +
-      "<br />" +
-       "<br />" +
-       "<i>" +
-        output.content +
-          "</i>" +
-          "<br />" +
-          "<br />" +
-          "<br />" +
-          "<br />";
-    document.body.appendChild(post);
+    var content = output.content;
+    console.log(`txid is ${threadid} `);
+    console.log(`message is ${content}`);
+    var thread = document.createElement("div");
+    thread.id = (`${threadid}`);
+    thread.innerHTML = output.content;
+  document.body.appendChild(thread)
   })
 })
 
