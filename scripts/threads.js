fce065c8aa069debdc7507e6e61c3a16e97e26c2
Jacob Sitowski <jacob.sitowski@gmail.com>
2019-07-24 12:30:45 -0400

    Added replies, temp replybutton

--- scripts/threads.js
+++ scripts/threads.js
@@ -6,12 +6,13 @@ var threads = {
     "out.s1": "13jgh5vQC5a43rvawKVourhCY7JiCaDpPH",
     "out.s2": "THREAD"
      },
-    "limit": 30,
+    "limit": 5,
   },
   "r": {
     "f": "[ .[] | { txid: .tx.h, timestamp: .blk.t?, txt: .out[0]?.s3, img: .out[0].s4 }]"
   }
 }
+
 
 // parse it
 var b64 = btoa(JSON.stringify(threads));
@@ -25,7 +26,6 @@ fetch(url, header).then(function(r) {
 
 }).then(function(r) {
   r.c.forEach(function(output) {
-
     var threadid = output.txid;
     var content = output.txt;
 
@@ -41,17 +41,66 @@ fetch(url, header).then(function(r) {
     img.setAttribute("src", `${url}`);
     console.log(`url is ${url}`);
 
-  //
+  // thread element
     thread.innerHTML =
-    "<p class=stamp>" + output.timestamp + "</p>" +
+    "<br />" +
+    "<a id='replybutton'>Reply</a>" +    
+    "<br />" +
+    "<br />" +
     "<img src=" + `${url}` + " class=pic></img>" +
     "<br />" +    "<br />" +
     "<p class=threadtext>" +
     "<br />" +
+    "<br />" +
+    "<br />" +
     output.txt + "</p>" +
-    "<br />";
+    "<br />" +
+    "<p class=replytext></p>";
     document.body.appendChild(thread);
+    var replies = {
+      "v": 3,
+      "q": {
+        "find": {
+        "out.s1": "13jgh5vQC5a43rvawKVourhCY7JiCaDpPH",
+        "out.s2": "REPLY",
+        "out.s5": threadid
+         },
+        "limit": 30,
+      },
+      "r": {
+        "f": "[ .[] | { replytxid: .tx.h, replytimestamp: .blk.t?, replytxt: .out[0]?.s3, replyimg: .out[0].s4, replyto: .out[0].s5 }]"
+      }
+    }
 
+    
+    // parse it
+    var b64 = btoa(JSON.stringify(replies));
+    var url = "https://genesis.bitdb.network/q/1FnauZ9aUH2Bex6JzdcV4eNX7oLSSEbxtN/" + b64;
+    var header = {
+      headers: { key: "14QX7pn5GbipWvNLyDfrdcZLzwvPYJSnhB" }
+    };
+    //
+    fetch(url, header).then(function(r) {
+      return r.json()
+    }).then(function(r) {
+      r.c.forEach(function(output) {
+        replyto = output.replyto;
+        replytxt = output.replytxt;
+        replytxid = output.replytxid;
+        console.log(replytxt + "//" + replytxid + "//" + "is a reply to" + replyto);
+        var reply = document.getElementById(`${replyto}`)
+        reply.id = (`${replytxid}`);
+        console.log(replytxt + "REPLY");
+        reply.querySelector(".replytext").innerHTML = 
+        "<br />" +
+        "<br />" +
+        "<br />" +
+        "<br />" +
+        replytxid +
+        "<br />" +
+        "<br />" +
+        replytxt;
 })
 })
-
+})
+})
